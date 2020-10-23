ae9de77cdad79b458c55510cc47829eb481d69fc
panda-info <panda@informain.com>
2020-10-23 10:32:52 +0100

    make swiper scrollable

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swiper.component.scss']
 })
 export class SwiperComponent implements OnInit {
+  public currentImageIndex = 3
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nextImage(): void {
+    this.currentImageIndex = ++this.currentImageIndex % 4;
+  }
+
+  previousImage(): void {
+    this.currentImageIndex = (--this.currentImageIndex + 4) % 4;
+  }
 }
