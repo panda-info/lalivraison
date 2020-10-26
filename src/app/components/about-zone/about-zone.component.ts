f347ce4c63722cbc15fefac54ff95f110f5c6992
panda-info <panda@informain.com>
2020-10-26 17:49:43 +0100

    wip about

---
+++ src/app/components/about-zone/about-zone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-zone',
+  templateUrl: './about-zone.component.html',
+  styleUrls: ['./about-zone.component.scss']
+})
+export class AboutZoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
